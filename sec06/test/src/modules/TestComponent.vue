d5214517c31067cdb7c13df4efd5169bbaf77fc0
taka01150810 <taka01150810@gmail.com>
2022-10-15 14:52:38 +0900

    SFC(SFC)

---
+++ sec06/test/src/modules/TestComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    テストです
+    {{ testData }}
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TestComponent',
+    data(){
+        return{
+            testData: 'テストDataです'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
