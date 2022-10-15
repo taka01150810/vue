ec03bdec4c9013364ee6b8595785a1a09a4b879b
taka01150810 <taka01150810@gmail.com>
2022-10-15 14:59:25 +0900

    SFC(Scoped CSS)

--- sec06/test/src/modules/TestComponent.vue
+++ sec06/test/src/modules/TestComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="red-b">
     テストです
     {{ testData }}
   </div>
@@ -16,6 +16,11 @@ export default {
 }
 </script>
 
-<style>
+<!-- scoped = 有効範囲を狭めることでこのクラス内だけで使えるCSSを作成できる-->
+<style scoped> 
+
+.red-b{
+  border:1px red solid;
+}
 
 </style>
