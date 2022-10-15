0a3c082a6e5f9f5dd39c8caff231bd5ae2c78b1c
taka01150810 <taka01150810@gmail.com>
2022-10-16 07:13:17 +0900

    VueRouter($routerと$route)

--- sec07/vuerouter/src/views/AboutView.vue
+++ sec07/vuerouter/src/views/AboutView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="checkRouteInfo">ルート情報</button><br>
+    <button @click="goToHome">Homeに戻る</button>
   </div>
 </template>
+<script>
+export default {
+  name: 'About',
+  methods: {
+    checkRouteInfo(){
+      console.log(this.$route)
+      console.log(this.$route.params.id)
+      console.log(this.$router)
+    },
+    goToHome(){
+      this.$router.push('/')
+    }
+  }
+}
+</script>
