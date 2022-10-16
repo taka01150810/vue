d703dbea31f10bf015c7eac4485dee0f4d941c9d
taka01150810 <taka01150810@gmail.com>
2022-10-16 10:22:27 +0900

    VueRouter(トランジションを含めたルート)

--- sec07/vuerouter/src/App.vue
+++ sec07/vuerouter/src/App.vue
@@ -11,13 +11,39 @@
       <router-link to="/user/profile">User</router-link>
     </nav>
     <div class="blue-b">
-      <router-view/>
+      <transition name="fade" mode="out-in">
+        <router-view/>
+      </transition>
     </div>
     <router-view name="sub"/>
   </div>
 </template>
 
-<style>
+<style lang="scss">
+.fade{
+  &-enter{
+    transform: translate(-100px, 0);
+    opacity: 0;
+    &-to{
+      opacity: 1;
+    }
+    &-active{
+      transition: all 1s 0s ease;
+    }
+  }
+  &-leave{
+    transform: translate(0, 0);
+    opacity: 1;
+    &-to{
+      transform: translate(100px, 0);
+      opacity: 0;
+    }
+    &-active{
+      transition: all .5s 0s ease;
+    }
+  }
+}
+
 .blue-b{
   border: 1px blue solid;
 }
