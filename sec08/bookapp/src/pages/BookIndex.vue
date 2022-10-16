4e04a0c00b755d4aba3bd96626e8d8b4fef9038b
taka01150810 <taka01150810@gmail.com>
2022-10-16 17:20:15 +0900

    SPA+GoogleBooksAPI(ファイルフォルダ整理)

---
+++ sec08/bookapp/src/pages/BookIndex.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>BookIndexです</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ sec08/bookapp/src/pages/BookSearch.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>BookSearchです</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
