cab7374a4a2475b14942d47609e51630ba04b5bf
taka01150810 <taka01150810@gmail.com>
2022-10-19 20:25:40 +0900

    SPA+CompositionAPU(beforeRouteEnter + $nextTick)

--- sec08/bookapp/src/pages/BookEdit.vue
+++ sec08/bookapp/src/pages/BookEdit.vue
@@ -1,6 +1,7 @@
 <template>
   <div>]
     BookEditです
+    {{ books[$route.params.id].title }}
     {{ books }}
   </div>
 </template>
@@ -10,7 +11,24 @@ export default {
   name: 'BookEdit',
   props:{
     books:Array
-  }
+  },
+  data(){
+    return{
+      book: ''
+    }
+  },
+  /*
+  next()の中にさらに$nextTickを使うと
+  DOM更新を待ってから処理する
+  next( vm => {}) で vmを thisのように使える
+  */
+  beforeRouteEnter(to, from, next){
+    next( vm =>{
+      //vmを通じてコンポーネントインスタンスにアクセス
+      vm.$nextTick(()=>{
+        vm.book = vm.books[vm.$route.params.id]
+      })
+    })}
 }
 </script>
 
