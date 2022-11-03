189a7b884c8f90888784d04d79609d7f9242eb5c
taka01150810 <taka01150810@gmail.com>
2022-11-03 16:24:49 +0900

    Vue3(エントリーポイントの確認)

--- sec10/vue3-test/src/main.js
+++ sec10/vue3-test/src/main.js
@@ -1,6 +1,8 @@
+//必要な機能を指定
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// createAppはアプリケーションAPIをチェーンで繋げる事ができる
 createApp(App).use(store).use(router).mount('#app')
