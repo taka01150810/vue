b869834b7338fc3fd86c7529b6a2e605646ce241
taka01150810 <taka01150810@gmail.com>
2022-11-03 11:45:42 +0900

    Vuex(Vuex stateとmutations)

--- sec09/vuex/src/store/index.js
+++ sec09/vuex/src/store/index.js
@@ -4,11 +4,17 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  //初期値
   state: {
+    count: 0
   },
   getters: {
   },
+  //mutationの役割はstateの値を変更する
   mutations: {
+    increment( state ){ //statue引数は約束事
+      state.count++
+    }
   },
   actions: {
   },
--- sec09/vuex/src/views/HomeView.vue
+++ sec09/vuex/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
+  <!-- 子供ページ -->
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
