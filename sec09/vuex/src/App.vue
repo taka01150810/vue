82fd6aa4b133882d31f251b507348d3a31dbcb4a
taka01150810 <taka01150810@gmail.com>
2022-11-03 12:22:38 +0900

    Vuex(getters)

--- sec09/vuex/src/App.vue
+++ sec09/vuex/src/App.vue
@@ -7,5 +7,27 @@
     </nav>
     <router-view/>
     {{ $store.state.count }}
+    <ul>
+      <li v-for="user in visibleUsers" :key="user.id">
+        {{ user.id }} : {{ user.name }} : {{ user.isVisible }}
+      </li>
+      <br>
+      {{ getUserById }}<br>
+      {{ getUserById.name }}
+    </ul>
   </div>
 </template>
+
+<script>
+export default({
+  computed:{
+    visibleUsers(){
+      return this.$store.getters.visibleUsers
+    },
+    getUserById(){
+      return this.$store.getters.getUserById(2)
+    }
+  },
+})
+</script>
+
--- sec09/vuex/src/store/index.js
+++ sec09/vuex/src/store/index.js
@@ -6,9 +6,27 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   //初期値
   state: {
-    count: 0
+    count: 0,
+    users:[
+      {id: 1, name: 'レブロン', isVisible: true},
+      {id: 2, name: 'KD', isVisible: false},
+      {id: 3, name: 'やにす', isVisible: true},
+    ]
   },
   getters: {
+    /*
+    visibleUsers( state ){
+      //条件 ... isVisibleがtrueだったら表示する
+      return state.users.filter( user => user.isVisible)
+    }
+    */
+   //アロー関数で書く場合
+   visibleUsers: state => state.users.filter( user => user.isVisible),
+
+   //メソッドスタイル
+   getUserById: state => id => {
+    return state.users.find( user => user.id === id )
+   }
   },
   //mutationの役割はstateの値を変更する
   mutations: {
