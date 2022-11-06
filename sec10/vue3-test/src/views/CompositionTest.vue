3c5b87f06a4e33d7fe32f9ee25dc5ab22ae2b400
taka01150810 <taka01150810@gmail.com>
2022-11-06 13:43:43 +0900

    Vue3(setup()の戻り値)

--- sec10/vue3-test/src/views/CompositionTest.vue
+++ sec10/vue3-test/src/views/CompositionTest.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     Compositionテストです
+    <p>{{ name }}</p>
+    <p>{{ age }}</p>
   </div>
 </template>
 
@@ -8,12 +10,15 @@
 export default {
     data(){},
     setup(){
-        console.log('setupです')
-        console.log(this) //結果 undefined
-        /*
-        Setup関数内はthisを使わない
-        ->アロー関数が使いやすい
-        */
+        let name = '大谷'
+        const age = 30
+        
+        return {
+            name,
+            age
+        } 
+        //returnに書いた変数・関数をtemplate内で扱える
+        //しかしリアクティブではない
     },
     created(){
         console.log('createdです')
