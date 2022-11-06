dfda5b9c15862d4371c1ff5e79fc6aa28ec73e42
taka01150810 <taka01150810@gmail.com>
2022-11-06 13:59:44 +0900

    Vue3(ref())

--- sec10/vue3-test/src/views/CompositionTest.vue
+++ sec10/vue3-test/src/views/CompositionTest.vue
@@ -3,19 +3,32 @@
     Compositionテストです
     <p>{{ name }}</p>
     <p>{{ age }}</p>
+    <p>{{ nameRef }}</p>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
     data(){},
     setup(){
         let name = '大谷'
         const age = 30
+        /* ref リアクティブな変数
+        1つの値をリアクティブにしたい場合に使う
+        値を参照オブジェクトでラップすることで
+        リアクティブな状態にする
+        */
+        const nameRef = ref('レブロン')
+
+        console.log(nameRef)//結果 オブジェクトの値
+        console.log(nameRef.value)//結果 レブロン
         
         return {
             name,
-            age
+            age,
+            nameRef
         } 
         //returnに書いた変数・関数をtemplate内で扱える
         //しかしリアクティブではない
