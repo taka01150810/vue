709c3d41deeb8bea5e0db91a4e83ea4e2eab92c3
taka01150810 <taka01150810@gmail.com>
2022-11-07 20:20:32 +0900

    Vue3(script setupを実際に試してみる)

--- sec11/script_setup_test/src/App.vue
+++ sec11/script_setup_test/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ScriptSetupTest />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ScriptSetupTest from './components/ScriptSetupTest.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ScriptSetupTest
   }
 }
 </script>
---
+++ sec11/script_setup_test/src/components/ScriptSetupTest.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+const increment = () => {
+    count.value++;
+};
+const decrement = () => {
+ count.value--;
+};
+</script>
+
+<template>
+
+<h1>{{ count }}</h1>
+<button @click="increment">Incremenボタン</button>
+<button @click="decrement">Decrementボタン</button>
+
+</template>
