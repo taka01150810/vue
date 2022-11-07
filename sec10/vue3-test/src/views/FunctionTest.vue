9caa894d1da1255433ff70f37c479bcc92223a4b
taka01150810 <taka01150810@gmail.com>
2022-11-07 19:17:26 +0900

    Vue3(合成関数(script内))

--- sec10/vue3-test/src/views/FunctionTest.vue
+++ sec10/vue3-test/src/views/FunctionTest.vue
@@ -1,10 +1,45 @@
 <template>
-  <div>Functionテストです</div>
+<div>
+    <p>商品名:{{ item.name }}</p>
+    <p>単価:{{ item.price }}</p>
+    <p>合計: {{ totalPrice }}</p>
+    <div>数量</div>
+    <button @click='decrement'>-</button>
+    <button @click='increment'>+</button>
+</div>
 </template>
 
 <script>
-export default {
+import { reactive } from '@vue/reactivity'
+import { computed } from '@vue/runtime-core'
 
+//合成関数
+const useCounter = item => {
+    const increment = () => {
+        item.amount++
+    }
+    const decrement = () => {
+        item.amount--
+    }
+    const totalPrice = computed(()=>{
+        return item.price * item.amount
+    })
+
+    return{ increment, decrement, totalPrice }
+}
+
+export default {
+    setup(){
+        const item = reactive({
+            name: '商品名',
+            price: 100,
+            amount: 0
+        })
+
+        const{ increment, decrement, totalPrice } = useCounter(item)
+
+        return{ item, increment, decrement, totalPrice }
+    },
 }
 </script>
 
